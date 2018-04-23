edccc2e97974d7da595b07df3575898e3d188502
Kulonful <37340779+kulonful@users.noreply.github.com>
2018-04-23 22:50:54 +0300

    add functional
    Based work.
    Tests

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
-a = input()
-print(a * 3)
+versionChat = '0.1.1'
+print("# Author's: kulonful, pufit\n# Socket's-TCP chat\n# [!] Server [!]\n# [?] Version " + str(versionChat) + " [?]")
+
+import socket as s
+import sys as system
+
+socket = s.socket()
+clients = []
+
+def acceptUser(wsclient, address):
+    clients.append([wsclient, address])
+    print('/ new user connected /')
+
+class serverSettings():
+    port = int(input('[Settings] Input port: '))
+    if port < 2000:
+        print('Invalid port (2000 - 8000)')
+        system.exit()
+    elif port > 8000:
+        print('Invalid port (2000 - 8000)')
+        system.exit()
+    valueClients = int(input('[Settings] Input int value of people which can connected at moment: '))
+    if valueClients < 1:
+        print("Clients can't be so small [> 1]")
+
+print("Server settings:\n\tport = " + str(serverSettings.port) + '\n\tmax clients: ' + str(serverSettings.valueClients))
+if input('Input [Y] for accept or [N] for decline: ').upper() == 'N':
+    print('Starting canceled by admin')
+    system.exit()
+
+socket.bind(('', serverSettings.port))
+socket.listen(serverSettings.valueClients)
+
+print('Try start server')
+
+while True:
+    wsclient, address = socket.accept()
+    acceptUser(wsclient, address)
+    data = socket.recv(1024).decode()
+    print(data)
